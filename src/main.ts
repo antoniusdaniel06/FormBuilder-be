fa83af177453d2df83267ea46b118471f368bc33
antoniusdaniel06 <danilnainggolan10@gmail.com>
2026-03-01 01:06:02 +0700

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -4,26 +4,60 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
+// ✅ Swagger
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // CORS (lebih aman kalau spesifik)
+  // CORS
   app.enableCors({
-  origin: '*',
-});
+    origin: '*',
+  });
 
-  // Validation
-  app.useGlobalPipes(new ValidationPipe());
+  
+  // Global Validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // remove property yang tidak ada di DTO
+      forbidNonWhitelisted: true, // error kalau ada property asing
+      transform: true, 
+    }),
+  );
 
-  // Static folder (uploads)
+  // Static Folder (Uploads)
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads/',
   });
 
-  // Port dynamic (PENTING untuk production)
+  // Swagger Setup
+  const config = new DocumentBuilder()
+    .setTitle('Form Builder API')
+    .setDescription('API Documentation for Form Builder Project')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'Authorization',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'access-token', // name reference
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); 
+  
+  // Dynamic Port
+
   const PORT = process.env.PORT || 3000;
 
   await app.listen(PORT);
   console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`📘 Swagger running on http://localhost:${PORT}/api`);
 }
+
 bootstrap();
